8b56e79be96b92d09edf46ab5367b1975e772ad4
nezuo <hinckleytmicah@gmail.com>
2024-03-10 16:14:11 -0700

    Simplify boolean logic

--- src/client/Benches/Booleans.lua
+++ src/client/Benches/Booleans.lua
@@ -3,7 +3,7 @@ local random = Random.new(908248)
 local data = {}
 
 for _ = 1, 1000 do
-	table.insert(data, if random:NextInteger(0, 1) == 0 then false else true)
+	table.insert(data, random:NextInteger(0, 1) == 1)
 end
 
 return data
